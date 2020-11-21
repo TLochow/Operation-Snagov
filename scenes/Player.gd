5fde46c648b8ebd1726e1cbf0c389462ea2481af
Torben Lochow <t.lochow@gmx.de>
2020-11-21 19:50:36 +0100

    Shooting Base Function

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,10 +1,20 @@
 extends KinematicBody2D
 
+var SHOTSCENE = preload("res://scenes/Shot.tscn")
+
+onready var ShotNode = get_tree().get_nodes_in_group("ShotsNode")[0]
+
 onready var AnimPlayer = $AnimationPlayer
 var PlayingWalkAnimation = true
 onready var PlayerSprite = $Sprite
 
 var LookDirection = Vector2(0.0, 0.0)
+
+var ShootCooldownTime = 0.5
+var ShootCooldown = 0.0
+var ShotSpread = 0.1
+var ShotAmount = 1.0
+var ShotDamage = 1.0
 
 func _ready():
 	AnimPlayer.play("Walk")
@@ -29,5 +39,20 @@ func _physics_process(delta):
 			AnimPlayer.play()
 			PlayingWalkAnimation = true
 	
-	LookDirection = (.get_global_mouse_position() - get_position()).normalized()
-	PlayerSprite.rotation = LookDirection.angle()
+	var pos = get_position()
+	LookDirection = (.get_global_mouse_position() - pos).normalized()
+	var lookAngle = LookDirection.angle()
+	PlayerSprite.rotation = lookAngle
+	
+	ShootCooldown -= delta
+	if Input.is_action_pressed("mouse_left") and ShootCooldown <= 0.0:
+		var shootPos = pos + (LookDirection * 6.0)
+		Shoot(lookAngle, shootPos)
+
+func Shoot(angle, pos):
+	ShootCooldown = ShootCooldownTime
+	for i in range(ShotAmount):
+		var shot = SHOTSCENE.instance()
+		ShotNode.add_child(shot)
+		shot.set_position(pos)
+		shot.Shoot(ShotDamage, angle + rand_range(-ShotSpread, ShotSpread))
---
+++ scenes/Shot.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+var Shot = false
+var ShotEnd = Vector2(0.0, 0.0)
+
+var Line
+
+func Shoot(damage, angle):
+	Shot = true
+	var direction = Vector2(cos(angle), sin(angle))
+	var localPos = to_local(get_position())
+	Line = $Line2D
+	var raycast = $RayCast2D
+	raycast.cast_to = direction * 1000.0
+	raycast.force_raycast_update()
+	if raycast.is_colliding():
+		var hitPoint = raycast.get_collision_point()
+		ShotEnd = to_local(hitPoint)
+		Line.points = [localPos, localPos]
+		var collider = raycast.get_collider()
+		if collider.has_method("Damage"):
+			collider.Damage(damage, angle)
+	else:
+		ShotEnd = to_local(raycast.cast_to)
+		Line.points = [localPos, localPos]
+	
+	var colorTween = $ColorTween
+	colorTween.interpolate_property(Line, "default_color", Color(0.5, 0.5, 0.5, 0.5), Color(0.5, 0.5, 0.5, 0.0), 0.2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	colorTween.start()
+
+func _process(delta):
+	if Shot:
+		var lineEndPos = Line.points[1]
+		var maxLength = ShotEnd.distance_to(lineEndPos)
+		lineEndPos += (ShotEnd - lineEndPos).normalized() * min(delta * 7000.0, maxLength)
+		Line.points[1] = lineEndPos
+
+func _on_ColorTween_tween_all_completed():
+	queue_free()
