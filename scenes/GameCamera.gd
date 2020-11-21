7e55dd7f016cba8f749cbd016d86865ff03baa24
Torben Lochow <t.lochow@gmx.de>
2020-11-21 16:55:30 +0100

    Camera Movement accounts for UI space.

--- scenes/GameCamera.gd
+++ scenes/GameCamera.gd
@@ -5,16 +5,15 @@ onready var Player = get_tree().get_nodes_in_group("Player")[0]
 func _process(delta):
 	var pos = get_position()
 	var relativePlayerPos = Player.get_position() - pos
-	print(relativePlayerPos)
 	if relativePlayerPos.x < -200.0:
 		pos.x -= 400.0
 		set_position(pos)
 	elif relativePlayerPos.x > 200.0:
 		pos.x += 400.0
 		set_position(pos)
-	if relativePlayerPos.y < -113.0:
-		pos.y -= 226.0
+	if relativePlayerPos.y < -87.0:
+		pos.y -= 200.0
 		set_position(pos)
 	if relativePlayerPos.y > 113.0:
-		pos.y += 226.0
+		pos.y += 200.0
 		set_position(pos)
