c233f4059215510f399f819e94b2253b6466c88f
Torben Lochow <t.lochow@gmx.de>
2020-12-14 00:20:53 +0100

    Added Fullscreen switch and new icon.

--- Settings.gd
+++ Settings.gd
@@ -5,6 +5,8 @@ signal Back
 var MusicVolume = 0.0
 var SoundEffectsVolume = 0.0
 
+var Fullscreen = true
+
 var Loading = true
 
 func _ready():
@@ -17,15 +19,19 @@ func LoadSettings():
 	if result == OK:
 		MusicVolume = config.get_value("settings", "music_volume", 0.0)
 		SoundEffectsVolume = config.get_value("settings", "sound_effects_volume", 0.0)
+		Fullscreen = config.get_value("settings", "fullscreen", true)
 	SetAudioVolume()
+	OS.window_fullscreen = Fullscreen
 	$Sound/Music/MusicSlider.value = MusicVolume
 	$Sound/Effects/EffectsSlider.value = SoundEffectsVolume
+	$Sound/Control/FullscreenCheckbox.pressed = Fullscreen
 
 func SaveSettings():
 	var config = ConfigFile.new()
 	var result = config.load("user://settings.cfg")
 	config.set_value("settings", "music_volume", MusicVolume)
 	config.set_value("settings", "sound_effects_volume", SoundEffectsVolume)
+	config.set_value("settings", "fullscreen", Fullscreen)
 	config.save("user://settings.cfg")
 
 func SetAudioVolume():
@@ -41,6 +47,10 @@ func _on_EffectsSlider_value_changed(value):
 	SetAudioVolume()
 	if not $Sound/Effects/SoundEffectDemo.playing and not Loading:
 		$Sound/Effects/SoundEffectDemo.play()
+
+func _on_FullscreenCheckbox_pressed():
+	Fullscreen = $Sound/Control/FullscreenCheckbox.pressed
+	OS.window_fullscreen = Fullscreen
 
 func _on_SettingsBack_pressed():
 	SaveSettings()
