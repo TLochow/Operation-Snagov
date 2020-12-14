eb4a78c531b1bad402b235a318fa4f86de959cdd
Torben Lochow <t.lochow@gmx.de>
2020-12-14 22:38:17 +0100

    Fixed Bug on splashscreen, when load time is long.

--- SplashScreen.gd
+++ SplashScreen.gd
@@ -1,11 +1,14 @@
 extends Control
 
+var Redirected = false
+
 func _ready():
 	var timeSinceStart = OS.get_ticks_msec()
 	var restTimeMsec = max(2000.0 - timeSinceStart, 1.0)
 	$Timer.wait_time = restTimeMsec / 1000.0
 	$Timer.start()
-	print(restTimeMsec)
 
 func _on_Timer_timeout():
-	SceneChanger.ChangeScene("res://MainMenu.tscn")
+	if not Redirected:
+		Redirected = true
+		SceneChanger.ChangeScene("res://MainMenu.tscn")
