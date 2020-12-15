11cf76f1f8a6305b653c0d2a311de80b6a1ee370
TLochow <42862435+TLochow@users.noreply.github.com>
2020-12-15 09:33:31 +0100

    Grenades can only explode once.

--- scenes/shots/Grenade.gd
+++ scenes/shots/Grenade.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 var EXPLOSIONSCENE = preload("res://scenes/shots/Explosion.tscn")
 
+var Exploded = false
 var ExplodeOnContact = false
 
 func _on_Timer_timeout():
@@ -14,7 +15,9 @@ func _on_Grenade_body_entered(body):
 		$Bounce.play()
 
 func Explode(pos, strength):
-	var explosion = EXPLOSIONSCENE.instance()
-	explosion.set_position(get_position())
-	get_parent().call_deferred("add_child", explosion)
-	call_deferred("queue_free")
+	if not Exploded:
+		Exploded = true
+		var explosion = EXPLOSIONSCENE.instance()
+		explosion.set_position(get_position())
+		get_parent().call_deferred("add_child", explosion)
+		call_deferred("queue_free")
